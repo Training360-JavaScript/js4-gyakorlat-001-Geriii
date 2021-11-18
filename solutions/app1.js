90f2a8f4b03de95588f577fb4f655404a6713aa5
Geriii <k.geri6@gmail.com>
2021-11-18 08:47:38 +0100

    all tests passed

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,7 +1,15 @@
+const converterPromise = (array) => {
+  const result = new Promise((resolve,reject) => {
+    if (array.every(item => typeof item === "string")){
+      resolve(array.map(
+        (item) => item.toUpperCase()
+        ) 
+      );
+    } else {
+      reject('Error: Not all elements are string type!')
+    };
+  });
+  return result;
+};
 
-
-
-
-
-
-// export default converterPromise;
+export default converterPromise;
